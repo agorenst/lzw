725e9971495abc976221bdab78766a3cb88ccb6d
Aaron Gorenstein <agorenst@outlook.com>
2019-05-09 08:28:40 -0400

    The length updating was still the issue!
    I spent a while retyping everything, but ultimatey I realized what the
    actual issue was. I have a somewhat hacky implementation, but it works!

--- Makefile
+++ Makefile
@@ -1,19 +1,26 @@
 CC=clang
 CFLAGS=-Wall -Werror -g -fsanitize=address,undefined -std=c99 -pedantic
+#CFLAGS=-Wall -Werror -g -std=c99 -pedantic
 
 lzw:
+
+test: lzw
+	cat lzw.c | ./lzw -e | ./lzw -d | diff lzw.c -
+
+debugfiles: debug_encoding.lzw debug_decoding.txt
+	diff debug_encoding.lzw debug_decoding.txt | more
 
 test_encoding.lzw: lzw
 	./lzw -e < lzw.c > $@
 
 debug_encoding.lzw: lzw
-	./lzw -e -g < lzw.c > $@
+	./lzw -e -g < lzw.c > $@ 2>&1
 
 test_decoding.txt: test_encoding.lzw
 	./lzw -d < $< > $@
 
 debug_decoding.txt: test_encoding.lzw
-	./lzw -d -g < $< > $@
+	./lzw -d -g < $< > $@ 2>&1
 
 clean:
 	rm -f lzw *.o *~ test_encoding.lzw test_decoding.txt
--- lzw.c
+++ lzw.c
@@ -54,32 +54,38 @@ void exercise_emit() {
 }
 
 typedef struct lzw_node_tag {
-  int key;
+  uint32_t key;
   struct lzw_node_tag* children[256];
 } lzw_node_t;
 typedef lzw_node_t* lzw_node_p;
 
-int lzw_length = 1;
-int lzw_next_key = 0;
+uint32_t lzw_length = 1;
+uint32_t lzw_next_key = 0;
 lzw_node_p root = NULL;
 lzw_node_p curr = NULL;
-char** lzw_key_to_str = NULL;
+uint8_t** lzw_key_to_str = NULL;
+uint32_t* lzw_key_to_len = NULL;
 
-// this is for debugging purposes
+void print_uint8_t_bin(uint8_t x) {
+  for (uint8_t i = 8; i >= 0; i--) {
+    fprintf(stderr, "%s", (x & (1 << i)) ? "1" : "0");
+  }
+}
+void print_uint32_t_bin(uint32_t x) {
+  for (int32_t i = 30; i >= 0; i--) {
+    fprintf(stderr, "%s", (x & (1 << i)) ? "1" : "0");
+  }
+}
+void print_uint8_t_array(uint8_t* a, uint32_t l) {
+  for (uint32_t i = 0; i < l; i++) {
+    fprintf(stderr, "0x%X", a[i]);
+  }
+}
 void debug_emit(uint32_t v, uint8_t l, FILE* o) {
-  for (uint8_t i = 0; i < l; ++i) {
-    uint32_t mask = 1 << (l - (i+1));
-    uint32_t bit = v & mask;
-    bit >>= (l-i)-1;
-    fputc(bit ? '1' : '0', o);
-  }
-  char debug_str[256];
-  strcpy(debug_str, lzw_key_to_str[v]);
-  for (int i = 0; i < strlen(debug_str); ++i) {
-    if (debug_str[i] == '\n') debug_str[i] = '_';
-  }
-  printf(" %s", debug_str);
-  fputc('\n', o);
+  print_uint32_t_bin(v);
+  fprintf(stderr, " ");
+  print_uint8_t_array(lzw_key_to_str[v], lzw_key_to_len[v]);
+  fprintf(stderr, "\n");
 }
 
 int biggest_one(int v) {
@@ -93,18 +99,22 @@ int biggest_one(int v) {
   return -1;
 }
 
+void debug_key(int key, int len);
 bool lzw_next_char(uint8_t c) {
   if (curr->children[c] == NULL) {
+    const uint32_t l = curr->key == -1 ? 0 : lzw_key_to_len[curr->key];
+    const uint32_t k = lzw_next_key++;
     curr->children[c] = calloc(1, sizeof(lzw_node_t));
-    curr->children[c]->key = lzw_next_key++;
+    curr->children[c]->key = k;
+    lzw_key_to_len[k] = l+1;
+    uint8_t** table_string = &lzw_key_to_str[k];
 
-    char* currString = curr->key == -1 ? "" : lzw_key_to_str[curr->key];
-    uint32_t l = strlen(currString);
-    char** table_string = &lzw_key_to_str[lzw_next_key-1];
-    *table_string = calloc(l+2, 1);
-    strcpy(*table_string, currString);
-    (*table_string)[l] = (char) c;
-    (*table_string)[l+1] = '\0';
+    *table_string = calloc(l+1, sizeof(uint8_t));
+    if (l) {
+      uint8_t* currString = lzw_key_to_str[curr->key];
+      memcpy(*table_string, currString, l*sizeof(uint8_t));
+    }
+    (*table_string)[l] = c;
 
     // we DON'T update curr here.
     return true;
@@ -115,15 +125,31 @@ bool lzw_next_char(uint8_t c) {
   }
 }
 
-bool update_length() {
-  if (biggest_one(lzw_next_key) > lzw_length) {
-    //printf("Updating length! %d %d -> %d\n", lzw_next_key, lzw_length, lzw_length+1);
+void do_len_update() {
+    fprintf(stderr, "Updating length! %d %d -> %d\n", lzw_next_key, lzw_length, lzw_length+1);
     lzw_length++;
-    char** new_buff = calloc(1<<lzw_length, sizeof(char*));
-    memcpy(new_buff, lzw_key_to_str, sizeof(char*)*(1<<(lzw_length-1)));
-    char** t = lzw_key_to_str;
+
+    {
+    uint8_t** new_buff = calloc(1<<lzw_length, sizeof(uint8_t*));
+    memcpy(new_buff, lzw_key_to_str, sizeof(uint8_t*)*(1<<(lzw_length-1)));
+    uint8_t** t = lzw_key_to_str;
     lzw_key_to_str = new_buff;
     free(t);
+    }
+
+    {
+    uint32_t* new_buff = calloc(1<<lzw_length, sizeof(uint32_t));
+    memcpy(new_buff, lzw_key_to_len, sizeof(uint32_t)*(1<<(lzw_length-1)));
+    uint32_t* t = lzw_key_to_len;
+    lzw_key_to_len = new_buff;
+    free(t);
+    }
+
+}
+
+bool update_length() {
+  if (biggest_one(lzw_next_key) > lzw_length) {
+    do_len_update();
     return true;
   }
   assert(lzw_length >= biggest_one(lzw_next_key));
@@ -134,13 +160,14 @@ void init_lzw() {
   root = (lzw_node_p) calloc(1, sizeof(lzw_node_t));
   curr = root;
   root->key = -1;
-  lzw_key_to_str = calloc(1<<lzw_length, sizeof(char*));
-  for (int i = 0; i < 256; ++i) {
-    lzw_next_char((char)i);
+  lzw_key_to_str = calloc(1<<lzw_length, sizeof(uint8_t*));
+  lzw_key_to_len = calloc(1<<lzw_length, sizeof(uint32_t));
+  for (uint8_t i = 0; i < (uint8_t)(i+1); ++i) {
+    lzw_next_char(i);
     update_length();
   }
   fprintf(stderr, "lzw_length = %d\n", lzw_length);
-  assert(lzw_length == 9);
+  //assert(lzw_length == 9);
 }
 
 void encode() {
@@ -150,6 +177,7 @@ void encode() {
   while (c != EOF) {
     bool newString = lzw_next_char(c);
     if (newString) {
+      if (debugMode) fprintf(stderr, "capping char: 0x%X\n", (uint8_t) c);
       if (debugMode) debug_emit(curr->key, lzw_length, stdout);
       else emit(curr->key, lzw_length, stdout);
       curr = root;
@@ -178,8 +206,9 @@ bool readbits(uint32_t* v, uint8_t l, FILE* i) {
       if (readBufferLength != 0) {
         fprintf(stderr, "Error, nonempty read buffer on EOF: %X %d\n",
             readBuffer, readBufferLength);
+        readBuffer <<= (8 - readBufferLength);
+        readBufferLength = 8;
       }
-      assert(readBufferLength == 0);
       return false;
     }
     else {
@@ -195,8 +224,9 @@ bool readbits(uint32_t* v, uint8_t l, FILE* i) {
   readBufferCopy >>= (readBufferLength - l);
   //fprintf(stderr, "readBufferCopy after slide (%d): %X\n", l - readBufferLength, readBufferCopy);
   // erase the topmost bits
-  readBufferCopy <<= MAX_BUFFER_LEN - l;
-  readBufferCopy >>= MAX_BUFFER_LEN - l;
+  readBufferCopy &= (1 << l) - 1;
+  //readBufferCopy <<= MAX_BUFFER_LEN - l;
+  //readBufferCopy >>= MAX_BUFFER_LEN - l;
   //fprintf(stderr, "readBufferCopy after erase: %X\n", readBufferCopy);
   // copy the result!
   *v = readBufferCopy;
@@ -220,69 +250,50 @@ bool lzw_valid_key(uint32_t k) {
 }
 
 void debug_key(int key, int len) {
-  int mask = 1 << (len-1);
-  for (int i = 0; i < len; ++i) {
-    if (mask & key) {
-      printf("1");
-    }
-    else {
-      printf("0");
-    }
-    mask >>= 1;
-  }
-  char debug_str[256];
-  strcpy(debug_str, lzw_key_to_str[key]);
-  for (int i = 0; i < strlen(debug_str); ++i) {
-    if (debug_str[i] == '\n') debug_str[i] = '_';
-  }
-  printf(" %s", debug_str);
-  printf("\n");
+  debug_emit(key, len, stderr);
 }
 
 void decode() {
   init_lzw();
 
-  uint32_t currKey, nextKey;
-  readbits(&currKey, lzw_length, stdin);
-  if (debugMode) debug_key(currKey, lzw_length);
-  assert(biggest_one(currKey) <= 8);
-  char* currString = lzw_key_to_str[currKey];
-  assert(strlen(currString) == 1);
-
   for (;;) {
-    for (int i = 0; i < strlen(currString); ++i) {
+    uint32_t currKey;
+    readbits(&currKey, lzw_length, stdin);
+    uint8_t* currString = lzw_key_to_str[currKey];
+    const uint32_t l = lzw_key_to_len[currKey];
+    for (int i = 0; i < l; ++i) {
       if (!debugMode) fputc(currString[i], stdout);
-      bool newString = lzw_next_char(currString[i]);
-      assert(!newString);
-      fflush(stdout);
+      bool b = lzw_next_char(currString[i]);
+      assert(!b);
     }
-    bool newBits = readbits(&nextKey, lzw_length, stdin);
-    if (!newBits) break;
 
-    char newChar = '\0';
-    bool refresh = false;
+    uint32_t nextKey;
+
+    // we have to update the length in anticipation.
+    if (biggest_one(lzw_next_key+1) > lzw_length) {
+      do_len_update();
+    }
+
+    bool nextBits = readbits(&nextKey, lzw_length, stdin);
+
+    uint8_t* nextString = currString;
     if (lzw_valid_key(nextKey)) {
-      newChar = lzw_key_to_str[nextKey][0];
+      nextString = lzw_key_to_str[nextKey];
     }
-    else {
-      printf("invalid key!\n");
-      refresh = true;
-      newChar = currString[0];
-    }
-    bool newString = lzw_next_char(newChar);
-    assert(newString);
-    refresh |= update_length();
+
+    uint8_t c = nextString[0];
+    lzw_next_char(c);
+    if (debugMode) fprintf(stderr, "capping char: 0x%X\n", c);
+    if (debugMode) debug_key(currKey, lzw_length);
+    // put the newkey back.
+    pushbits(nextKey, lzw_length);
+    bool uplen = update_length();
+    assert(!uplen);
     curr = root;
 
-    // we have to look up the key as the new length
-    if (refresh) {
-      printf("refreshing!\n");
-      pushbits(nextKey, lzw_length-1);
-      readbits(&nextKey, lzw_length, stdin);
+    if (!nextBits) {
+      break;
     }
-    assert(lzw_valid_key(nextKey));
-    if (debugMode) debug_key(nextKey, lzw_length);
-    currString = lzw_key_to_str[nextKey];
   }
 }
 
